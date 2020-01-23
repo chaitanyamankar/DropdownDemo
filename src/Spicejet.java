c1d9a7c6f59a03422b98b30b4fbea28a353831c7
chaitanyamankar <chaitanya.mankar@gmail.com>
2020-01-23 11:37:54 +0530

    Dyanamic Selection

--- src/Spicejet.java
+++ src/Spicejet.java
@@ -15,6 +15,13 @@ public class Spicejet {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.spicejet.com/default.aspx");
 		driver.manage().window().maximize();
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='IXU']")).click();;
+		Thread.sleep(1000);
+		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+		//driver.findElement(By.className("ui-state-default ui-state-highlight ui-state-active")).click();;
+				
 		driver.findElement(By.id("divpaxinfo")).click();
 		
 		for (int i = 0; i < 3; i++) {
